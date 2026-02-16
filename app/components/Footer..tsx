a559e29acd27135b1d8ac0ee76de27571dade441
Jordan Baker <Jordan.Baker@likewize.com>
2026-02-16 14:48:39 +0000

    Shift the GitHub footer element to the right and set element width boundaries

--- app/components/Footer..tsx
+++ app/components/Footer..tsx
@@ -3,15 +3,15 @@ import { RiGithubFill, RiLinkedinFill } from "@remixicon/react";
 const Footer = () => {
     return (
         <footer className="w-full flex border-t border-t-[#314147]">
-            <h4 className="inline-block px-6 py-4 border-r border-r-[#314147]">find me in:</h4>
-            <ul className="flex">
-                <li className="last:justify-end">
-                    <a className="p-4 inline-flex border-r border-r-[#314147] last:border-l last:border-l-[#314147] last:border-r-0" href="https://www.linkedin.com/in/jordan-baker-b0b871205/">
+            <h4 className="inline-block px-6 py-4 border-r border-r-[#314147] min-w-40">find me in:</h4>
+            <ul className="list-none w-full">
+                <li className="inline-flex last:float-right border-r border-r-[#314147] last:border-l last:border-l-[#314147] last:border-r-0">
+                    <a className="p-4 inline-flex" href="https://www.linkedin.com/in/jordan-baker-b0b871205/">
                         <RiLinkedinFill />
                     </a>
                 </li>
-                <li className="last:justify-end">
-                    <a className="p-4 lg:py-4 lg:px-8 inline-flex gap-2.5 border-r border-r-[#314147] last:border-l last:border-l-[#314147] last:border-r-0" href="https://www.linkedin.com/in/jordan-baker-b0b871205/">
+                <li className="inline-flex last:float-right border-r border-r-[#314147] last:border-l last:border-l-[#314147] last:border-r-0">
+                    <a className="p-4 lg:py-4 lg:px-8 inline-flex gap-2.5" href="https://www.linkedin.com/in/jordan-baker-b0b871205/">
                         <span className="hidden lg:block">@JordanBaker852</span>
                         <RiGithubFill />
                     </a>
